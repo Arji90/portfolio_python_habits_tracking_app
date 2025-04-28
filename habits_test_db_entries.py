75612b446f6f6f6cbc158de8dce475a6722a0a85
Arthur <arhut.j.merz@gmail.com>
2025-04-28 11:59:45 +0200

    change habits_test_db_entries.py
    adjust dates for 4-week data requirement
    add table display for DB-entries

--- habits_test_db_entries.py
+++ habits_test_db_entries.py
@@ -5,6 +5,7 @@ test script to set up databases for presentation
 import sqlite3
 import os
 import json
+from tabulate import tabulate
 from datetime import datetime, timedelta
 from helper import const_handler
 
@@ -38,15 +39,16 @@ def main():
         __date_today__ = datetime.today()
         __date_8weeks_ago__ = __date_today__ - timedelta(days=56)
         __date_7weeks_ago__ = __date_today__ - timedelta(days=49)
+        __date_6weeks_ago__ = __date_today__ - timedelta(days=42)
         __date_4weeks_ago__ = __date_today__ - timedelta(days=28)
         __date_2weeks_ago__ = __date_today__ - timedelta(days=14)
         __date_1week_ago__ = __date_today__ - timedelta(days=7)
-        __date_2days_ago__ = __date_today__ - timedelta(days=2)
         __date_yesterday__ = __date_today__ - timedelta(days=1)
 
         __pet_walk_history__ = {}
         for i in range(8): # break for __date_8weeks_ago__ to __date_7weeks_ago__ (both including) for last month breaks
             current_date = __date_8weeks_ago__ + timedelta(days=i)
+            # if the test-data is created shortly after new year, date handling is always a pain
             year = current_date.year
             month = current_date.month
 
@@ -59,13 +61,13 @@ def main():
         habit_1 = (1, 'Quit smoking', '', Const.get_periodicity_daily(),
                    str(__date_4weeks_ago__), '', str(__date_2weeks_ago__), 13, 13, '{}')
         habit_2 = (1, 'Go for a walk', "It doesn't matter how long", Const.get_periodicity_daily(),
-                   str(__date_2weeks_ago__), '', str(__date_yesterday__), 13, 13, '{}')
+                   str(__date_6weeks_ago__), '', str(__date_yesterday__), 41, 41, '{}')
         habit_3 = (1, 'Vacuum', 'Vacuum all rooms', Const.get_periodicity_weekly(),
-                   str(__date_2weeks_ago__), '', str(__date_1week_ago__), 1, 1, '{}')
+                   str(__date_7weeks_ago__), '', str(__date_1week_ago__), 6, 6, '{}')
         habit_4 = (1, 'Taking your pet for a walk', "but it's a goldfish...", Const.get_periodicity_daily(),
                    str(__date_8weeks_ago__), str(__date_7weeks_ago__), '', 0, 0, json.dumps(__pet_walk_history__))
         habit_5 = (2, 'Go for a walk', 'Or not, who cares', Const.get_periodicity_daily(),
-                   str(__date_2weeks_ago__), '', '', 0, 0, '{}')
+                   str(__date_8weeks_ago__), '', '', 0, 0, '{}')
 
         sql_add_test_habits_values = (habit_1, habit_2, habit_3, habit_4, habit_5)
 
@@ -79,6 +81,12 @@ def main():
         connection.commit()
         cursor.executemany(sql_add_test_users_query, sql_add_test_users_values)
         connection.commit()
+        created_users = cursor.execute("select * from users").fetchall()
+        print()
+        # added for transparency reason
+        print('created users for testing:')
+        print(tabulate(created_users, headers=['id', 'name', 'deactivated']))
+        print()
         connection.close()
 
         # update habits
@@ -91,6 +99,13 @@ def main():
         connection.commit()
         cursor.executemany(sql_add_test_habits_query, sql_add_test_habits_values)
         connection.commit()
+        created_habits = cursor.execute("select * from habits").fetchall()
+        print()
+        # added for transparency reason
+        print('created habits for testing:')
+        print(tabulate(created_habits, headers=['id', 'user-id', 'title', 'description', 'periodicity',
+                                                'created date', 'deactivated', 'last break', 'last checked',
+                                                'steak', 'longest streak', 'break history']))
         connection.close()
 
 main()
